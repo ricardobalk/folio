c4491d1b678d11c44527a06607d151c1a7b5cd71
Ricardo Balk <14904229+ricardobalk@users.noreply.github.com>
2020-11-26 00:08:28 +0100

    Added Dockerfile

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ricardobalk/folio
+
+## Docker
+
+1) Building the headless Scribus container
+
+```bash
+docker build -t scribus-headless .
+```
+
+2) Generating the PDF
+
+```bash
+mkdir -p export/
+docker run -it -v "$PWD/:/home/scribus/" scribus-headless ./Portfolio.sla -g --python-script sla-to-pdf.py
+```
+
+3) That's it. PDF file should be available in `export/`.
--- sla-to-pdf.py
+++ sla-to-pdf.py
@@ -2,9 +2,9 @@ import os
 import scribus
 
 if scribus.haveDoc() :
-    filename = os.path.splitext(scribus.getDocName())[0]
+    filename = os.path.dirname(scribus.getDocName())+"/export/"+os.path.basename(scribus.getDocName().split('.')[0])+".pdf"
     pdf = scribus.PDFfile()
-    pdf.file = filename+".pdf"
+    pdf.file = filename
     pdf.save()
 else :
     print("No file open")
